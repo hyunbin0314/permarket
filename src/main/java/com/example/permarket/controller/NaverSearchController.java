4650e24406d293d69317728fb867d597ef8e478c
LimHyunbin <flrstsalang@naver.com>
2024-07-03 15:48:05 +0900

    Controller update

--- src/main/java/com/example/permarket/controller/NaverSearchController.java
+++ src/main/java/com/example/permarket/controller/NaverSearchController.java
@@ -1,6 +1,19 @@
 package com.example.permarket.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/news/search")
 public class NaverSearchController {
+
+    @GetMapping
+    public String getSearch() {
+
+        return "";
+    }
 
 
 }
